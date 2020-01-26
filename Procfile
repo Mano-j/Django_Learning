930679d84fe6fb8829b3a38027a7868ed405a3e1
Mano-j <manojm.mailid@gmail.com>
2020-01-26 23:40:23 +0530

    added requirements and procfile'

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn flames-calcualtor:wsgi --log-file - 
--- trydjango/settings.py
+++ trydjango/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'trydjango.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'trydjango.wsgi.application'
 
 
 # Database
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
